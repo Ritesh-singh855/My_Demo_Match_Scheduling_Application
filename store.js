c71c7cababb046d5930c5069d204cc28c1693fa5
Ritesh-singh855 <61587968+Ritesh-singh855@users.noreply.github.com>
2024-04-04 10:01:30 +0530

    Delete store.js

--- store.js
+++
@@ -1,7 +0,0 @@
-// store.js
-import { createStore } from 'redux';
-import rootReducer from './reducers'; // You need to define your reducers
-
-const store = createStore(rootReducer);
-
-export default store;
